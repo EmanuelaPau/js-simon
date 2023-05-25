325d61f39ef023a9aa1d9e0476559a4f94dc2794
EmanuelaPau <uapame@gmail.com>
2023-05-25 15:21:55 +0200

    Add function to get current time

--- script/my_script.js
+++ script/my_script.js
@@ -1,1 +1,20 @@
 console.log('hello');
+
+function getUserDateAndTime() {
+    // I get the date
+    const today = new Date();
+    // I get the hours
+    const userHours = today.getHours();
+    // I get the minutes
+    const userMinutes = today.getMinutes();
+    // I get the seconds
+    const userSeconds = today.getSeconds();
+
+    // Create an array with hours, minutes and seconds.
+    const userDateList = [userHours, userMinutes, userSeconds];
+    console.log(userDateList);
+
+    return userDateList;
+}
+
+console.log(getUserDateAndTime);
